aff681f990e9616990d2d85893d8d6b97411da91
Mamduh Alassi <mamduhala@mellanox.com>
2019-12-03 10:02:51 +0200

    Add restor old guid with unit test

--- pkg/sriov/sriov.go
+++ pkg/sriov/sriov.go
@@ -70,6 +70,32 @@ func (p *pciUtilsImpl) GetPciAddress(ifName string, vf int) (string, error) {
 	return utils.GetPciAddress(ifName, vf)
 }
 
+// RebindVf unbind then bind the vf
+func (p *pciUtilsImpl) RebindVf(pfName, vfPciAddress string) error {
+	pfHandle, err := sriovnet.GetPfNetdevHandle(pfName)
+	if err != nil {
+		return err
+	}
+	var vf *sriovnet.VfObj
+	found := false
+	for _, vfObj := range pfHandle.List {
+		if vfObj.PciAddress == vfPciAddress {
+			vf = vfObj
+			found = true
+		}
+	}
+	if !found {
+		return fmt.Errorf("failed to find VF %s for PF %s", vfPciAddress, pfName)
+	}
+	if err = sriovnet.UnbindVf(pfHandle, vf); err != nil {
+		return err
+	}
+	if err = sriovnet.BindVf(pfHandle, vf); err != nil {
+		return err
+	}
+	return nil
+}
+
 type sriovManager struct {
 	nLink types.NetlinkManager
 	utils types.PciUtils
@@ -108,28 +134,6 @@ func (s *sriovManager) SetupVF(conf *types.NetConf, podifName string, cid string
 	// 3. Change netns
 	if err := s.nLink.LinkSetNsFd(linkObj, int(netns.Fd())); err != nil {
 		return fmt.Errorf("failed to move IF %s to netns: %q", tempName, err)
-	}
-
-	// 4. Set VF GUID
-	if conf.GUID != "" {
-		if len(conf.GUID) < 8 {
-			return fmt.Errorf("invalid guid %s", conf.GUID)
-		}
-		hwAddr, err := net.ParseMAC(conf.GUID)
-		if err != nil {
-			return fmt.Errorf("failed to parse Vf user's config GUID %s: %v", conf.GUID, err)
-		}
-		pfLink, err := s.nLink.LinkByName(conf.Master)
-		if err != nil {
-			return fmt.Errorf("failed to lookup master %q: %v", conf.Master, err)
-		}
-		if err = netlink.LinkSetVfNodeGUID(pfLink, conf.VFID, hwAddr); err != nil {
-			return fmt.Errorf("failed to set vf port guid: %v", err)
-		}
-
-		if err = netlink.LinkSetVfPortGUID(pfLink, conf.VFID, hwAddr); err != nil {
-			return fmt.Errorf("failed to set vf port guid: %v", err)
-		}
 	}
 
 	if err := netns.Do(func(_ ns.NetNS) error {
@@ -200,42 +204,6 @@ func (s *sriovManager) ApplyVFConfig(conf *types.NetConf) error {
 		return fmt.Errorf("failed to lookup master %q: %v", conf.Master, err)
 	}
 
-	// Set link guid
-	if conf.GUID != "" {
-		guid, err := net.ParseMAC(conf.GUID)
-		if err != nil {
-			return fmt.Errorf("failed to parse guid %s: %v", conf.GUID, err)
-		}
-		if err = s.nLink.LinkSetVfNodeGUID(pfLink, conf.VFID, guid); err != nil {
-			return fmt.Errorf("failed to add node guid %s: %v", guid, err)
-		}
-		if err = s.nLink.LinkSetVfPortGUID(pfLink, conf.VFID, guid); err != nil {
-			return fmt.Errorf("failed to add port guid %s: %v", guid, err)
-		}
-		// unbind vf then bind it to apply the new guid
-		pfHandle, err := sriovnet.GetPfNetdevHandle(conf.Master)
-		if err != nil {
-			return err
-		}
-		var vf *sriovnet.VfObj
-		found := false
-		for _, vfObj := range pfHandle.List {
-			if vfObj.PciAddress == conf.DeviceID {
-				vf = vfObj
-				found = true
-			}
-		}
-		if !found {
-			return fmt.Errorf("failed to find VF %s for PF %s", conf.DeviceID, conf.Master)
-		}
-		if err = sriovnet.UnbindVf(pfHandle, vf); err != nil {
-			return err
-		}
-		if err = sriovnet.BindVf(pfHandle, vf); err != nil {
-			return err
-		}
-	}
-
 	// Set link state
 	if conf.LinkState != "" {
 		var state uint32
@@ -252,6 +220,36 @@ func (s *sriovManager) ApplyVFConfig(conf *types.NetConf) error {
 		}
 		if err = s.nLink.LinkSetVfState(pfLink, conf.VFID, state); err != nil {
 			return fmt.Errorf("failed to set vf %d link state to %d: %v", conf.VFID, state, err)
+		}
+	}
+
+	// Set link guid
+	if conf.GUID != "" {
+		if len(conf.GUID) < 22 || conf.GUID == "00:00:00:00:00:00:00:00" {
+			return fmt.Errorf("invalid guid %s", conf.GUID)
+		}
+		// save link guid
+		vfLink, err := s.nLink.LinkByName(conf.HostIFNames)
+		if err != nil {
+			return fmt.Errorf("failed to lookup vf %q: %v", conf.HostIFNames, err)
+		}
+
+		conf.HostIFGUID = vfLink.Attrs().HardwareAddr.String()[36:]
+
+		// Set link guid
+		guid, err := net.ParseMAC(conf.GUID)
+		if err != nil {
+			return fmt.Errorf("failed to parse guid %s: %v", conf.GUID, err)
+		}
+		if err = s.nLink.LinkSetVfNodeGUID(pfLink, conf.VFID, guid); err != nil {
+			return fmt.Errorf("failed to add node guid %s: %v", guid, err)
+		}
+		if err = s.nLink.LinkSetVfPortGUID(pfLink, conf.VFID, guid); err != nil {
+			return fmt.Errorf("failed to add port guid %s: %v", guid, err)
+		}
+		// unbind vf then bind it to apply the new guid
+		if err = s.utils.RebindVf(conf.Master, conf.DeviceID); err != nil {
+			return err
 		}
 	}
 
@@ -273,6 +271,24 @@ func (s *sriovManager) ResetVFConfig(conf *types.NetConf) error {
 		// accommodate for drivers / NICs that don't support the netlink command (e.g. igb driver)
 		if err = s.nLink.LinkSetVfState(pfLink, conf.VFID, 0); err != nil {
 			return fmt.Errorf("failed to set link state to auto for vf %d: %v", conf.VFID, err)
+		}
+	}
+
+	// Reset link guid
+	if conf.HostIFGUID != "" && conf.HostIFGUID != "00:00:00:00:00:00:00:00" {
+		guid, err := net.ParseMAC(conf.HostIFGUID)
+		if err != nil {
+			return fmt.Errorf("failed to parse guid %s: %v", conf.HostIFGUID, err)
+		}
+		if err = s.nLink.LinkSetVfNodeGUID(pfLink, conf.VFID, guid); err != nil {
+			return fmt.Errorf("failed to add node guid %s: %v", guid, err)
+		}
+		if err = s.nLink.LinkSetVfPortGUID(pfLink, conf.VFID, guid); err != nil {
+			return fmt.Errorf("failed to add port guid %s: %v", guid, err)
+		}
+		// unbind vf then bind it to apply the guid
+		if err = s.utils.RebindVf(conf.Master, conf.DeviceID); err != nil {
+			return err
 		}
 	}
 
--- pkg/sriov/sriov_test.go
+++ pkg/sriov/sriov_test.go
@@ -9,6 +9,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/mock"
 	"github.com/vishvananda/netlink"
+	"net"
 )
 
 // FakeLink is a dummy netlink struct used during testing
@@ -30,6 +31,72 @@ func (l *FakeLink) Type() string {
 
 var _ = Describe("Sriov", func() {
 
+	Context("Checking ApplyVFConfig function", func() {
+		var (
+			netconf *types.NetConf
+		)
+
+		BeforeEach(func() {
+			netconf = &types.NetConf{
+				Master:      "i4",
+				DeviceID:    "0000:af:06.0",
+				VFID:        0,
+				HostIFNames: "i1",
+			}
+		})
+
+		It("ApplyVFConfig without GUID", func() {
+			mocked := &mocks.NetlinkManager{}
+
+			fakeLink := &FakeLink{netlink.LinkAttrs{}}
+
+			mocked.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+			mocked.On("LinkSetVfState", fakeLink, mock.AnythingOfType("int"), mock.AnythingOfType("unit32")).Return(nil)
+			sm := sriovManager{nLink: mocked}
+			err := sm.ApplyVFConfig(netconf)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("ApplyVFConfig with valid GUID", func() {
+			mockedNetLinkManger := &mocks.NetlinkManager{}
+			mockedPciUtils := &mocks.PciUtils{}
+
+			gid, err := net.ParseMAC("00:00:04:a5:fe:80:00:00:00:00:00:00:11:22:33:00:00:aa:bb:cc")
+			Expect(err).ToNot(HaveOccurred())
+
+			fakeLink := &FakeLink{netlink.LinkAttrs{
+				HardwareAddr: gid,
+			}}
+			netconf.GUID = "01:23:45:67:89:ab:cd:ef"
+
+			mockedNetLinkManger.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+			mockedNetLinkManger.On("LinkSetVfNodeGUID", fakeLink, mock.AnythingOfType("int"), mock.Anything).Return(nil)
+			mockedNetLinkManger.On("LinkSetVfPortGUID", fakeLink, mock.AnythingOfType("int"), mock.Anything).Return(nil)
+
+			mockedPciUtils.On("RebindVf", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
+
+			sm := sriovManager{nLink: mockedNetLinkManger, utils: mockedPciUtils}
+			err = sm.ApplyVFConfig(netconf)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("ApplyVFConfig with invalid GUID", func() {
+			mockedNetLinkManger := &mocks.NetlinkManager{}
+			mockedPciUtils := &mocks.PciUtils{}
+
+			gid, err := net.ParseMAC("00:00:04:a5:fe:80:00:00:00:00:00:00:11:22:33:00:00:aa:bb:cc")
+			Expect(err).ToNot(HaveOccurred())
+
+			fakeLink := &FakeLink{netlink.LinkAttrs{
+				HardwareAddr: gid,
+			}}
+			netconf.GUID = "invalid GUID"
+
+			mockedNetLinkManger.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+
+			sm := sriovManager{nLink: mockedNetLinkManger, utils: mockedPciUtils}
+			err = sm.ApplyVFConfig(netconf)
+			Expect(err).To(HaveOccurred())
+		})
+	})
 	Context("Checking SetupVF function", func() {
 		var (
 			podifName string
@@ -76,45 +143,101 @@ var _ = Describe("Sriov", func() {
 			err = sm.SetupVF(netconf, podifName, contID, targetNetNS)
 			Expect(err).NotTo(HaveOccurred())
 		})
-		Context("Checking ReleaseVF function", func() {
-			var (
-				podifName string
-				contID    string
-				netconf   *types.NetConf
-			)
+	})
+	Context("Checking ReleaseVF function", func() {
+		var (
+			podifName string
+			contID    string
+			netconf   *types.NetConf
+		)
 
-			BeforeEach(func() {
-				podifName = "net1"
-				contID = "dummycid"
-				netconf = &types.NetConf{
-					Master:      "ib0",
-					DeviceID:    "0000:af:06.0",
-					VFID:        0,
-					HostIFNames: "ib1",
-					ContIFNames: "net1",
+		BeforeEach(func() {
+			podifName = "net1"
+			contID = "dummycid"
+			netconf = &types.NetConf{
+				Master:      "ib0",
+				DeviceID:    "0000:af:06.0",
+				VFID:        0,
+				HostIFNames: "ib1",
+				ContIFNames: "net1",
+			}
+		})
+		It("Assuming existing interface", func() {
+			var targetNetNS ns.NetNS
+			targetNetNS, err := testutils.NewNS()
+			defer func() {
+				if targetNetNS != nil {
+					targetNetNS.Close()
 				}
-			})
-			It("Assuming existing interface", func() {
-				var targetNetNS ns.NetNS
-				targetNetNS, err := testutils.NewNS()
-				defer func() {
-					if targetNetNS != nil {
-						targetNetNS.Close()
-					}
-				}()
-				Expect(err).NotTo(HaveOccurred())
-				mocked := &mocks.NetlinkManager{}
-				fakeLink := &FakeLink{netlink.LinkAttrs{Index: 1000, Name: "dummylink"}}
+			}()
+			Expect(err).NotTo(HaveOccurred())
+			mocked := &mocks.NetlinkManager{}
+			fakeLink := &FakeLink{netlink.LinkAttrs{Index: 1000, Name: "dummylink"}}
 
-				mocked.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
-				mocked.On("LinkSetDown", fakeLink).Return(nil)
-				mocked.On("LinkSetName", fakeLink, mock.Anything).Return(nil)
-				mocked.On("LinkSetNsFd", fakeLink, mock.AnythingOfType("int")).Return(nil)
-				mocked.On("LinkSetUp", fakeLink).Return(nil)
-				sm := sriovManager{nLink: mocked}
-				err = sm.ReleaseVF(netconf, podifName, contID, targetNetNS)
-				Expect(err).NotTo(HaveOccurred())
-			})
+			mocked.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+			mocked.On("LinkSetDown", fakeLink).Return(nil)
+			mocked.On("LinkSetName", fakeLink, mock.Anything).Return(nil)
+			mocked.On("LinkSetNsFd", fakeLink, mock.AnythingOfType("int")).Return(nil)
+			mocked.On("LinkSetUp", fakeLink).Return(nil)
+			sm := sriovManager{nLink: mocked}
+			err = sm.ReleaseVF(netconf, podifName, contID, targetNetNS)
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+	Context("Checking ResetVFConfig function", func() {
+		var (
+			netconf *types.NetConf
+		)
+
+		BeforeEach(func() {
+			netconf = &types.NetConf{
+				Master:      "i4",
+				DeviceID:    "0000:af:06.0",
+				VFID:        0,
+				HostIFNames: "i1",
+			}
+		})
+
+		It("ResetVFConfig without GUID", func() {
+			mocked := &mocks.NetlinkManager{}
+
+			fakeLink := &FakeLink{netlink.LinkAttrs{}}
+
+			mocked.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+			mocked.On("LinkSetVfState", fakeLink, mock.AnythingOfType("int"), mock.AnythingOfType("unit32")).Return(nil)
+			sm := sriovManager{nLink: mocked}
+			err := sm.ResetVFConfig(netconf)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("ResetVFConfig with valid GUID", func() {
+			mockedNetLinkManger := &mocks.NetlinkManager{}
+			mockedPciUtils := &mocks.PciUtils{}
+
+			fakeLink := &FakeLink{netlink.LinkAttrs{}}
+			netconf.HostIFGUID = "01:23:45:67:89:ab:cd:ef"
+
+			mockedNetLinkManger.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+			mockedNetLinkManger.On("LinkSetVfNodeGUID", fakeLink, mock.AnythingOfType("int"), mock.Anything).Return(nil)
+			mockedNetLinkManger.On("LinkSetVfPortGUID", fakeLink, mock.AnythingOfType("int"), mock.Anything).Return(nil)
+
+			mockedPciUtils.On("RebindVf", mock.AnythingOfType("string"), mock.AnythingOfType("string")).Return(nil)
+
+			sm := sriovManager{nLink: mockedNetLinkManger, utils: mockedPciUtils}
+			err := sm.ResetVFConfig(netconf)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("ResetVFConfig with invalid GUID", func() {
+			mockedNetLinkManger := &mocks.NetlinkManager{}
+			mockedPciUtils := &mocks.PciUtils{}
+
+			fakeLink := &FakeLink{netlink.LinkAttrs{}}
+			netconf.HostIFGUID = "invalid GUID"
+
+			mockedNetLinkManger.On("LinkByName", mock.AnythingOfType("string")).Return(fakeLink, nil)
+
+			sm := sriovManager{nLink: mockedNetLinkManger, utils: mockedPciUtils}
+			err := sm.ResetVFConfig(netconf)
+			Expect(err).To(HaveOccurred())
 		})
 	})
 })
